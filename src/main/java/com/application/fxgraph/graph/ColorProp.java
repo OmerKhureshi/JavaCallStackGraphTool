8120e432ecfde17d8ee936035788e46337a1380f
omerkhureshi <syedomersalar@gmail.com>
2018-05-06 12:41:52 -0700

    changed color scheme

--- src/main/java/com/application/fxgraph/graph/ColorProp.java
+++ src/main/java/com/application/fxgraph/graph/ColorProp.java
@@ -10,10 +10,8 @@ public class ColorProp {
     public final static Color ENABLED = Color.DIMGRAY;
     public final static Color GREEN = Color.LIMEGREEN;
     public final static Color GREY = Color.DIMGRAY;
-    public final static Color RED = Color.RED;
+    public final static Color RED = Color.valueOf("#FF4C4C");
     public final static Color BLACK = Color.BLACK;
-
-
 
     // public final static Color POSITIVE_GREEN = Color.LIMEGREEN;
     // public final static Color INFO_BLUE = Color.SKYBLUE;
